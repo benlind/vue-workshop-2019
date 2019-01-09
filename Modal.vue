cfbbda552415b9bd7d62b99e963516162f552317
Steve Grazzini <steve.grazzini@grantstreet.com>
2019-01-09 09:49:27 +0900

    Move title/body/footer to data
    This just adds "data" attributes for the title, body, and footer.
    Once these are in place, you can use the Vue DevTools plugin to
    see the reactivity at work. (Just change those data attributes in
    DevTools, and see the component update itself automatically.)

--- Modal.vue
+++ Modal.vue
@@ -2,9 +2,21 @@
   <div
     class="w-100 h-100 position-absolute d-flex justify-content-center align-items-center">
     <div class="card">
-      <div class="card-header">Title</div>
-      <div class="card-body">Body</div>
-      <div class="card-footer">Footer</div>
+      <div class="card-header">{{ title }}</div>
+      <div class="card-body">{{ body }}</div>
+      <div class="card-footer">{{ footer }}</div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title:   'Title (data)',
+      body:    'Body (from data)',
+      footer:  'Footer (from data)',
+    }
+  }
+}
+</script>
